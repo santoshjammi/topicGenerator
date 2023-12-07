c29658b23d7ae9bd02f7db1cd3f28548ea879a96
santoshjammi <santoshjammi@gmail.com>
2023-12-08 00:49:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,11 @@ else:
             new_kw = f'{kw} {letter}'
             for se, getter in kw_getters.items():
 #                print(f'Exploring for {new_kw} in {se}')
-                kw_ses = SES.kw_results(getter, new_kw)
-                kws += kw_ses
+                try:
+                    kw_ses = SES.kw_results(getter, new_kw)
+                    kws += kw_ses
+                except Exception:
+                    pass
 #                print(f'{len(kw_ses)} keywords found in this step.\n')
         FW.write_txt(kws)
     with open(config.output_file, encoding='utf-8') as out_f:
